d142b49c8916851adb3c26b424fde4e8d7f26e48
Shengjie Xu <shengjiex98@gmail.com>
2023-03-15 20:32:07 -0400

    Add export for new functions

--- src/ControlTimingSafety.jl
+++ src/ControlTimingSafety.jl
@@ -18,7 +18,8 @@ include("safety.jl")
 export maximum_deviation_random, estimate_deviation
 include("probablesafety.jl")
 
-export schedule_xghtc, synthesize_constraints, estimate_constraints
+export schedule_xghtc, synthesize_constraints
+export estimate_constraints, verify_schedule, schedule_to_sequence
 include("schedule_synthesis.jl")
 
 end
